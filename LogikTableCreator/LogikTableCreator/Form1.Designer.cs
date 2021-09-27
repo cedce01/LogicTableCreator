8e3d4c4165d5a02c698d942a0127332792df4e1a
cedce01 <87926843+cedce01@users.noreply.github.com>
2021-09-27 12:52:08 +0200

    Add files via upload

---
+++ LogikTableCreator/LogikTableCreator/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace LogikTableCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // createButton
+            // 
+            this.createButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.createButton.Location = new System.Drawing.Point(12, 41);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(371, 57);
+            this.createButton.TabIndex = 0;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(371, 23);
+            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.Text = "A,B,C,D";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 110);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.createButton);
+            this.Name = "Form1";
+            this.Text = "LogicTable Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        internal System.Windows.Forms.Button createButton;
+    }
+}
+
---
+++ LogikTableCreator/LogikTableCreator/Form1.cs
@@ -0,0 +1,60 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LogikTableCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveFileDialog1.FileName = "table";
+            saveFileDialog1.DefaultExt = ".xlsx";
+            saveFileDialog1.AddExtension = true;
+            saveFileDialog1.InitialDirectory = path;
+            saveFileDialog1.Filter = "Excel (.xlsx)|*.xlsx";
+            saveFileDialog1.ShowDialog();
+            path = saveFileDialog1.FileName;
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Sample Sheet");
+                bool state = false;
+                int counter = 0;
+                int currentFreq;
+                int cases;
+                String[] inputs;
+                inputs = inputTextBox.Text.Split(',');
+                inputs = inputs.Reverse().ToArray();
+                cases = Convert.ToInt32(Math.Pow(inputs.Length, 2));
+                for (int j = 1; j < inputs.Length + 1; j++)
+                {
+                    currentFreq = Convert.ToInt32(Math.Pow(2, j - 1));
+                    worksheet.Cell(1, j).Value = inputs[j - 1];
+                    counter = 0;
+                    state = false;
+                    for (int i = 2; i < cases + 2; i++)
+                    {
+                        if (counter == currentFreq)
+                        {
+                            state = !state;
+                            counter = 0;
+                        }
+                        worksheet.Cell(i, inputs.Length - j + 1).Value = Convert.ToByte(state);
+                        counter++;
+                    }
+                }
+
+                workbook.SaveAs(path);
+            }
+            MessageBox.Show("done");
+
+        }
+    }
+}
